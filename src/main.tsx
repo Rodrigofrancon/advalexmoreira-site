d130dd8cfb146fd0ea92d7808911b3057fb03748
Rodrigo Franco <rodrigo.franconunes@gmail.com>
2026-08-06 18:53:46 -0300

    motordebusca

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 
 import "@fontsource/inter/400.css";
 import "@fontsource/inter/500.css";
@@ -15,6 +16,8 @@ import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </StrictMode>
 );
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,5 @@
+import { Helmet } from "react-helmet-async";
+
 import Hero from "@/components/home/Hero";
 import About from "@/components/home/About";
 import PracticeAreas from "@/components/home/PracticeAreas";
@@ -8,6 +10,22 @@ import Contact from "@/components/home/Contact";
 export default function Home() {
   return (
     <>
+      <Helmet>
+        <title>
+          Alex Moreira Advogados | Advocacia em Viamão, Porto Alegre e Região Metropolitana
+        </title>
+
+        <meta
+          name="description"
+          content="Escritório de advocacia com atendimento presencial em Viamão e online para todo o Rio Grande do Sul. Atuação em Direito Civil, Previdenciário, Trabalhista, Penal, do Consumidor e de Família."
+        />
+
+        <link
+          rel="canonical"
+          href="https://alexmoreiraadvogado.com.br/"
+        />
+      </Helmet>
+
       <Hero />
 
       <About />
