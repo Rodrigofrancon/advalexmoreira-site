db86f2af823c170420ef94dfb33132be3e82a505
Rodrigo Franco <rodrigo.franconunes@gmail.com>
2026-08-06 23:36:01 -0300

    segimg

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
+import { useEffect } from "react";
+
 import MainLayout from "./layouts/MainLayout";
 import Home from "./pages/Home";
 
 export default function App() {
+  useEffect(() => {
+    const blockContextMenu = (event: MouseEvent) => {
+      event.preventDefault();
+    };
+
+    const blockImageDrag = (event: DragEvent) => {
+      if (event.target instanceof HTMLImageElement) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener("contextmenu", blockContextMenu);
+    document.addEventListener("dragstart", blockImageDrag);
+
+    return () => {
+      document.removeEventListener("contextmenu", blockContextMenu);
+      document.removeEventListener("dragstart", blockImageDrag);
+    };
+  }, []);
+
   return (
     <MainLayout>
       <Home />
